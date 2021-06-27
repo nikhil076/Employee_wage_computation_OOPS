babb483996dd9754b2d4a28a9a37b3435fe99e60
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-27 12:03:54 +0530

    added method to check for attendance

--- src/com/bridgelabz/employeewage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -4,5 +4,18 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome  to Employeee wage computation program");
+		checkPresentOrAbsent();
+	}
+	public static boolean checkPresentOrAbsent() 
+	{
+		double random = Math.floor(Math.random()*10)%2;
+		if(random==1)
+		{
+			System.out.println("Employee is present");
+			return true;
+		}
+		else
+			System.out.println("Employee is absent");
+			return false;
 	}
 }
