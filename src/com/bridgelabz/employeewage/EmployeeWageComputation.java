2672745b7f077cd545c3c1eca55e91f44d4646c7
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-27 11:46:12 +0530

    Initializing the program

---
+++ src/com/bridgelabz/employeewage/EmployeeWageComputation.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeewage;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome  to Employeee wage computation program");
+	}
+}
